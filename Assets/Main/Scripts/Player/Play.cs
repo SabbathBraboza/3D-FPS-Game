e0e4b71d1a6b9da6fdb3c17e0dc5dbaa6428324f
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-06-24 15:12:06 +0530

    Made Changes in Scripts

--- Assets/Main/Scripts/Player/Play.cs
+++ Assets/Main/Scripts/Player/Play.cs
@@ -1,28 +1,58 @@
+using Emp37.Utility;
 using UnityEngine;
 
 namespace FPS.Player
 {
-    [HideInInspector]
-    public class Play : MonoBehaviour
-    {
-        [Header("References:")]
-        [SerializeField] private CharacterController Controller;
-        [SerializeField] private Transform Camera;
+      [HideInInspector]
+      public class Player : MonoBehaviour
+      {
+            [Header("References:")]
+            [SerializeField] private CharacterController Controller;
+            [SerializeField] private Transform Camera;
+            [SerializeField] private Aim aim;
+            [SerializeField] private Movement movement;
 
+            [Space(5f)]
+            [Header("Phyics:")]
+            [Min(1f)] public float Mass = 1f;
+            [Readonly] public Vector3 Force;
+            [Min(0f)] public float GravityScale = 1f;
+            private const float gravity = -9.81F;
 
-        private void Reset()
-        {
-            Controller = GetComponent<CharacterController>();
-            Camera = GetComponentInChildren<Camera>().transform;
-        }
+            public bool IsGrounded => Controller.isGrounded;
 
-        private void OnEnable() => Cursor.lockState = CursorLockMode.Locked;
-        private void OnDisable() => Cursor.lockState = CursorLockMode.None;
+            #region Editor
 
-        private void Update()
-        {
-            
-        }
+            private void Reset()
+            {
+                  Controller = GetComponent<CharacterController>();
+                  Camera = GetComponentInChildren<Camera>().transform;
+            }
+            #endregion
 
-    }
-}
+            #region RunTime
+            private void OnEnable()
+            {
+                  aim.enabled = true;
+                  Cursor.lockState = CursorLockMode.Locked;
+            }
+            private void Update()
+            {
+                  Force.y = IsGrounded ? -Mass : Force.y + GravityScale * gravity * Time.deltaTime;
+
+                  if (Input.GetButtonDown(KeyInput.Horizontal) || Input.GetButtonDown(KeyInput.Vertical))
+                  {
+                        movement.enabled = true;
+                  }
+                  Controller.Move((Force + movement.Pace) * Time.deltaTime);
+            }
+            private void OnDisable()
+            {
+                  Cursor.lockState = CursorLockMode.None;
+                  aim.enabled = false;
+            }
+            #endregion
+      }
+
+     }
+
