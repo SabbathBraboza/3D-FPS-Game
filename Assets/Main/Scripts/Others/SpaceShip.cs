f3aff5790ec93eae9af4cbf84e06416c7bf76694
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-07-05 12:00:01 +0530

    other spacecraft animation done

--- Assets/Main/Scripts/Others/SpaceShip.cs
+++ Assets/Main/Scripts/Others/SpaceShip.cs
@@ -5,11 +5,15 @@ public class SpaceShip : MonoBehaviour
 {
      [SerializeField] private Animator anime;
 
+    [Space(5f)]
+    [Header("Values")]
+    [SerializeField] private float landing;
+    [SerializeField] private float Flying;
       private void Reset() =>  anime = GetComponent<Animator>();
 
       private void Start()
       {
-            StartCoroutine(PlayLandingLoop());
+        StartCoroutine(PlayLandingLoop());
       }
 
       private IEnumerator PlayLandingLoop()
@@ -18,11 +22,11 @@ public class SpaceShip : MonoBehaviour
             {
                   anime.SetBool("Landing", false);
 
-               yield return new WaitForSeconds(12f);
+               yield return new WaitForSeconds(landing);
 
                   anime.SetBool("Landing", true);
 
-                  yield return new WaitForSeconds(20f); // Adjust delay as needed
+                  yield return new WaitForSeconds(Flying); // Adjust delay as needed
             };
       }
 }
