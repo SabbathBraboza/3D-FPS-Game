4d90c704833de983f5aeb6a0f2b2b2982cd8bfa0
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-07-09 21:14:07 +0530

    added New Script

---
+++ Assets/Main/Scripts/UI/WeaponUI.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace FPS.UI.Weapon
+{
+    public class WeaponUI : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text GunAmmo;
+        [SerializeField] private Image EqquipedGun;
+
+        private void Update()
+        {
+            
+        }
+    }
+}
