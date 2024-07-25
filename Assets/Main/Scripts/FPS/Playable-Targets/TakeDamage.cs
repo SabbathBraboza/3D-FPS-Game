891ab9ff9effc066ec85380a1fae5788aad02dbc
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-07-25 15:43:59 +0530

    gg

--- Assets/Main/Scripts/FPS/Playable-Targets/TakeDamage.cs
+++ Assets/Main/Scripts/FPS/Playable-Targets/TakeDamage.cs
@@ -6,18 +6,24 @@ public class TakeDamage : MonoBehaviour, IDamge
 
     [SerializeField] private int HP = 100;
     [SerializeField] private Animator anime;
+      private BoxCollider box;
 
     private void Reset()
     {
         anime = GetComponent<Animator>();
+            box = GetComponent<BoxCollider>();
     }
 
     public void Damage(int damage)
     {
         HP -= damage;
-        if (HP < damage) anime.SetTrigger("Dead");
+            if (HP < damage)
+            {
+                  anime.SetTrigger("Dead");
+                  box.enabled = false;
 
-        else anime.SetTrigger("Hurt");
+            }
+            else anime.SetTrigger("Hurt");
 
         print(HP);
     }
