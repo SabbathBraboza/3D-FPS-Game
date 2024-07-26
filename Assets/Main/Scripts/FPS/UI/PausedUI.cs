1987ab47cb45c43e2f518f62b784505cd640d844
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-07-26 17:14:34 +0530

    Pause UI Done

---
+++ Assets/Main/Scripts/FPS/UI/PausedUI.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PausedUI : MonoBehaviour
+{
+    [SerializeField] private GameObject PausePanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+           PausePanel.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0f;
+            
+        }  
+    }
+
+    public void ClosePause()
+    {
+      PausePanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+    }
+
+    public void Home()
+    {
+      Time.timeScale = 0f;
+      SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+      Application.Quit();
+      Debug.Log("Quiting");
+    }
+}
