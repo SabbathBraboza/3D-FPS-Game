168b4728f87c6b24722c5a0c6fa7c24e019908c4
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-08-05 12:01:44 +0530

    gg

---
+++ Assets/Main/Scripts/FPS/Environment/Training_Room.cs
@@ -0,0 +1,13 @@
+using FPS.Player;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Training_Room : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
+
+
+}
