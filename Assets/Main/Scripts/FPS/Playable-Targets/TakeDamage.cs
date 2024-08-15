2bbaea3a371a8a028ee9ccaafec4ddf744297b2b
SabbathBraboza <157228790+SabbathBraboza@users.noreply.github.com>
2024-08-15 21:32:24 +0530

    Many Thing Done So Can Explain

--- Assets/Main/Scripts/FPS/Playable-Targets/TakeDamage.cs
+++ Assets/Main/Scripts/FPS/Playable-Targets/TakeDamage.cs
@@ -1,24 +1,54 @@
+using Emp37.Utility;
 using TPS.Player;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TakeDamage : MonoBehaviour, IDamge
 {
-    [SerializeField] private int HP = 100;
+    [Header("Value:")]
+    [SerializeField] private int MaxHp = 100;
+
+    [Header("References:")]
     [SerializeField] private Animator anime;
     [SerializeField] private BoxCollider box;
-      [SerializeField] private ZombieController zombieController;
+    [SerializeField] private ZombieController zombieController;
+    [SerializeField] private NavMeshAgent Agent;
+
+    [Header("ReadOnly")]
+    [SerializeField,Readonly] private int CurrentHp;
+
+    private void Start() => CurrentHp = MaxHp;
+    
 
     public void Damage(int damage)
     {
-        damage = 25;
-        HP -= damage;
-        if (HP < damage)
-        {
-          zombieController.enabled = false;
-         anime.SetTrigger("Dead");
-          box.enabled = false;
-        }
-        else   anime.SetTrigger("Hurt");
+        damage = Mathf.Max(damage, 0);
+
+        CurrentHp -= damage;
+
+        if (CurrentHp < 0)
+            Die();
+
+        else
+            Hurt();
+
     }
- 
+
+    private void Hurt()
+    {
+        if(anime != null) anime.SetTrigger("Hurt");
+    }
+
+    private void Die()
+    {
+        if (anime != null) anime.SetTrigger("Dead");
+
+        if(box !=null) box.enabled = false;
+
+        if(zombieController != null) zombieController.enabled = false;
+
+        if(Agent != null) Agent.speed = 0;
+
+        Destroy(gameObject, 2f);
+    }
 }
